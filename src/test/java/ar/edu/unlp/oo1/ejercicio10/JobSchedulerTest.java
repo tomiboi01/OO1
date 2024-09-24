fd689bbc9430704e61600d35695ab7e396cbd98e
tomic <tomithespeedster@gmal.com>
2024-09-24 03:18:54 -0300

    ed

--- src/test/java/ar/edu/unlp/oo1/ejercicio10/JobSchedulerTest.java
+++ src/test/java/ar/edu/unlp/oo1/ejercicio10/JobSchedulerTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
 public class JobSchedulerTest {
     protected JobDescription firstJob;
     protected JobDescription highestPriorityJob;
